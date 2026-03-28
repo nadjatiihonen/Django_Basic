febd29b593f499f6f5908748af3ff2cffb1b937d
nadjatiihonen <nadja.toroponina@gmail.com>
2026-03-28 23:17:58 +0200

    Clear SECRET_KEY in settings.py
    Reset the SECRET_KEY to an empty string for security reasons.

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-c)$wrnp5dl7-c(hewy6z$hl^ktez4!v^=n#b=xx2^s7g4n$@ux"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
